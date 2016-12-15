d33e1a5abc4d66b9b30449529bc127f050cac9d4
Nicolai <nicolai@mmg.io>
2016-12-15 16:08:01 +0000

    Booleans added

--- quiz.js
+++ quiz.js
@@ -1,7 +1,13 @@
+var correctGuess = false;
 var randomNumber = Math.floor(Math.random() * 6) + 1;
 var guess = prompt('I am thinking of a number between 1 and 6. What is it?');
+
 if (parseInt(guess) === randomNumber) {
+ correctGuess = true;
+}
+
+if (correctGuess===true) {
  document.write('<p>You guessed the right number!</p>');
 } else {
- document.write('<p>Sorry, the number was ' + randomNumber + '</p>');
+ document.write('<p>Sorry. The number was ' + randomNumber + '.</p>');
 }
