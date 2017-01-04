4eec1a6d6a4348c8132097e99312006e9a483347
Nicolai <nicolai@mmg.io>
2017-01-04 15:10:55 +0000

    Random number generator quiz added

--- quiz.js
+++ quiz.js
@@ -4,6 +4,15 @@ var guess = prompt('I am thinking of a number between 1 and 6. What is it?');
 
 if (parseInt(guess) === randomNumber) {
  correctGuess = true;
+} else if (parseInt(guess) < randomNumber) {
+ var guessMore = prompt('Try again. The number I am thinking of is more than ' + guess)
+ if (parseInt(guessMore) === randomNumber) {
+  correctGuess = true;
+ }
+} else if (parseInt(guess) > randomNumber) {
+ var guessLess = prompt('Try again. The number I am thinking of is less than ' + guess)
+ if (parseInt(guessLess) === randomNumber) {
+  correctGuess = true;
 }
 
 if (correctGuess===true) {
